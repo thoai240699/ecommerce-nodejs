aed2d025f5960969936fdeeacfc882a176c06218
thoai240699 <thoai240699@gmail.com>
2025-06-18 06:25:28 +0700

    add controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('', (req, res, next) => {
-  return res.status(200).json({
-    message: 'Hello',
-  });
-});
+router.use('/v1/api',require('./access'))
+
+// router.get('', (req, res, next) => {
+//   return res.status(200).json({
+//     message: 'Hello',
+//   });
+// });
 
 module.exports = router;
